4006f258ce370d24d17a3dcf6fb40bd781c0b0cf
Krzysztof Benedyczak <golbi@icm.edu.pl>
2020-09-28 12:39:48 +0200

    FU-6 minimal spring boot based project skeleton

---
+++ furms-server/src/main/java/io/imunity/furms/server/FurmsServerApplication.java
@@ -0,0 +1,13 @@
+package io.imunity.furms.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FurmsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FurmsServerApplication.class, args);
+	}
+
+}
---
+++ furms-server/src/test/java/io/imunity/furms/server/FurmsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.imunity.furms.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FurmsServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
