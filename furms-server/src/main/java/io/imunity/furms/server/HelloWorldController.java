c879c4f47444c2d6d838f63ab0c7ef8a653ffa71
Krzysztof Benedyczak <golbi@icm.edu.pl>
2020-10-23 10:52:33 +0200

    align license

--- furms-server/src/main/java/io/imunity/furms/server/HelloWorldController.java
+++ furms-server/src/main/java/io/imunity/furms/server/HelloWorldController.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2020 Bixbit s.c. All rights reserved.
- * See LICENCE.txt file for licensing information.
+ * See LICENSE file for licensing information.
  */
 package io.imunity.furms.server;
 
