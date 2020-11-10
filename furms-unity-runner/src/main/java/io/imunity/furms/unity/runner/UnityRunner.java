0a250abb2f15f2668ca626db5e1661317af87ad3
Piotr Bihuniak <bihuniak.piotr@gmail.com>
2020-11-10 11:38:11 +0100

    FU-12 config

--- furms-unity-runner/src/main/java/io/imunity/furms/unity/runner/UnityRunner.java
+++ furms-unity-runner/src/main/java/io/imunity/furms/unity/runner/UnityRunner.java
@@ -4,24 +4,11 @@
  */
 package io.imunity.furms.unity.runner;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.stereotype.Component;
 import pl.edu.icm.unity.MessageSource;
-import pl.edu.icm.unity.engine.PKIManagementImpl;
 import pl.edu.icm.unity.engine.server.UnityApplication;
-import pl.edu.icm.unity.rest.jwt.authn.JWTVerificator;
 
-@SpringBootApplication
 public class UnityRunner
 {
-	@Bean
-	JWTVerificator addVerify(){
-		return new JWTVerificator(null);
-	}
-
 	public static void main(String[] args)
 	{
 		UnityApplication theServer = new UnityApplication(MessageSource.PROFILE_FAIL_ON_MISSING);
