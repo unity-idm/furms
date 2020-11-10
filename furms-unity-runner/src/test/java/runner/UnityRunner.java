b1425b667372477ba9b29555576669e2cb526912
Piotr Bihuniak <bihuniak.piotr@gmail.com>
2020-11-10 12:38:04 +0100

    FU-12 starter config

--- furms-unity-runner/src/test/java/runner/UnityRunner.java
+++ furms-unity-runner/src/test/java/runner/UnityRunner.java
@@ -2,7 +2,7 @@
  * Copyright (c) 2020 Bixbit s.c. All rights reserved.
  * See LICENSE file for licensing information.
  */
-package io.imunity.furms.unity.runner;
+package runner;
 
 import pl.edu.icm.unity.MessageSource;
 import pl.edu.icm.unity.engine.server.UnityApplication;
@@ -12,6 +12,6 @@ public class UnityRunner
 	public static void main(String[] args)
 	{
 		UnityApplication theServer = new UnityApplication(MessageSource.PROFILE_FAIL_ON_MISSING);
-		theServer.run(new String[] {"src/main/resources/unityServer.conf"});
+		theServer.run(new String[] {"src/test/resources/unityServer.conf"});
 	}
 }
