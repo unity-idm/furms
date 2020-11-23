54d4c18b6185071d392664035db0caed2bfbb75e
Piotr Bihuniak <bihuniak.piotr@gmail.com>
2020-11-23 19:44:46 +0100

    FU-13 fixes for CR-204

--- furms-unity-runner/src/test/java/io/imunity/furms/unity/runner/UnityRunner.java
+++ furms-unity-runner/src/test/java/io/imunity/furms/unity/runner/UnityRunner.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2020 Bixbit s.c. All rights reserved.
- *  See LICENSE file for licensing information.
+ * See LICENSE file for licensing information.
  */
 package io.imunity.furms.unity.runner;
 
