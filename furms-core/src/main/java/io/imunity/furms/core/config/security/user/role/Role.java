f3c7012b85b31a0f667936604dc366bf9747b7fa
Piotr Bihuniak <bihuniak.piotr@gmail.com>
2020-12-21 17:16:13 +0100

    FU-35 hot fix

--- furms-core/src/main/java/io/imunity/furms/core/config/security/user/role/Role.java
+++ furms-core/src/main/java/io/imunity/furms/core/config/security/user/role/Role.java
@@ -17,45 +17,47 @@ public enum Role {
 	FENIX_ADMIN(
 		"furmsFenixRole",
 		"ADMIN",
-			List.of(
-				AUTHENTICATED, PROFILE, SITE_READ, SITE_WRITE, COMMUNITY_READ, COMMUNITY_WRITE,
-				FENIX_ADMINS_MANAGEMENT
-			),
-			List.of(SITE_READ, SITE_WRITE, COMMUNITY_READ, COMMUNITY_WRITE)
+		List.of(
+			AUTHENTICATED, PROFILE, SITE_READ, SITE_WRITE, COMMUNITY_READ, COMMUNITY_WRITE,
+			FENIX_ADMINS_MANAGEMENT
 		),
-		FENIX_SUPPORT(
-			"furmsFenixRole",
-			"SUPPORT",
-			emptyList(),
-			emptyList()
+		List.of(
+			SITE_READ, SITE_WRITE, COMMUNITY_READ, COMMUNITY_WRITE
+		)
+	),
+	FENIX_SUPPORT(
+		"furmsFenixRole",
+		"SUPPORT",
+		emptyList(),
+		emptyList()
+	),
+	SITE_ADMIN(
+		"furmsSiteRole",
+		"SUPPORT",
+		List.of(
+			AUTHENTICATED, PROFILE, SITE_READ, SITE_WRITE, SITE_ADMINS_MANAGEMENT
 		),
-		SITE_ADMIN(
-			"furmsSiteRole",
-			"SUPPORT",
-			List.of(
-				AUTHENTICATED, PROFILE, SITE_READ, SITE_WRITE, SITE_ADMINS_MANAGEMENT
-			),
-			emptyList()
-		),
-		COMMUNITY_ADMIN(
-			"furmsCommunityRole",
-			"SUPPORT",
-			List.of(
+		emptyList()
+	),
+	COMMUNITY_ADMIN(
+		"furmsCommunityRole",
+		"SUPPORT",
+		List.of(
 			AUTHENTICATED, PROFILE, COMMUNITY_READ, COMMUNITY_WRITE, PROJECT_READ, PROJECT_WRITE,
 			COMMUNITY_ADMINS_MANAGEMENT
-			),
-			emptyList()
 		),
-		PROJECT_ADMIN(
-			"furmsProjectRole",
-			"SUPPORT",
-			List.of(
+		emptyList()
+	),
+	PROJECT_ADMIN(
+		"furmsProjectRole",
+		"SUPPORT",
+		List.of(
 			AUTHENTICATED, PROFILE, PROJECT_READ, PROJECT_WRITE, PROJECT_ADMINS_MANAGEMENT,
 			PROJECT_MEMBER_MANAGEMENT
-			),
-			emptyList()
 		),
-		PROJECT_MEMBER(
+		emptyList()
+	),
+	PROJECT_MEMBER(
 		"furmsProjectRole",
 		"SUPPORT",
 		List.of(
@@ -63,7 +65,6 @@ public enum Role {
 		),
 		emptyList()
 	);
-
 
 	public final String unityRoleAttribute;
 	public final String unityRoleValue;
@@ -77,7 +78,7 @@ public enum Role {
 		this.additionalCapabilities = List.copyOf(additionalCapabilities);
 	}
 
-	public static Role translateRole(String attributeType, String value){
+	public static Role translateRole(String attributeType, String value) {
 		return Arrays.stream(values())
 			.filter(r -> r.unityRoleAttribute.equals(attributeType))
 			.filter(r -> r.unityRoleAttribute.equals(value))
