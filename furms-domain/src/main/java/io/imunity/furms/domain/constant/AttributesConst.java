4452eba16e7d2a3ba6ff429a12dae0d181dba677
Piotr Piernik <piotrpiernik@gmail.com>
2021-01-26 15:18:29 +0100

    FU-25 user profile page

---
+++ furms-domain/src/main/java/io/imunity/furms/domain/constant/AttributesConst.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2021 Bixbit - Krzysztof Benedyczak. All rights reserved.
+ * See LICENCE.txt file for licensing information.
+ */
+
+package io.imunity.furms.domain.constant;
+
+public class AttributesConst{
+	public static final String EMAIL = "email";
+	public static final String FIRSTNAME = "firstname";
+	public static final String SURNAME = "surname";
+}
--- furms-web-ui/src/main/java/io/imunity/furms/ui/views/user_settings/ProfileView.java
+++ furms-web-ui/src/main/java/io/imunity/furms/ui/views/user_settings/ProfileView.java
@@ -5,13 +5,44 @@
 
 package io.imunity.furms.ui.views.user_settings;
 
+import static io.imunity.furms.domain.constant.RoutesConst.USER_BASE_LANDING_PAGE;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import com.google.common.base.Strings;
+import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.router.Route;
+
+import io.imunity.furms.core.config.security.user.FurmsUser;
+import io.imunity.furms.domain.constant.AttributesConst;
 import io.imunity.furms.ui.components.FurmsViewComponent;
 import io.imunity.furms.ui.components.PageTitle;
-
-import static io.imunity.furms.domain.constant.RoutesConst.USER_BASE_LANDING_PAGE;
 
 @Route(value = USER_BASE_LANDING_PAGE, layout = UserSettingsMenu.class)
 @PageTitle(key = "view.user-settings.profile.page.title")
 public class ProfileView extends FurmsViewComponent {
+
+	public ProfileView() {
+
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		FurmsUser user = (FurmsUser) authentication.getPrincipal();
+
+		FormLayout mainLayout = new FormLayout();
+		mainLayout.setSizeFull();
+		mainLayout.setResponsiveSteps(new FormLayout.ResponsiveStep("1em", 1));
+		String firstname = user.getAttribute(AttributesConst.FIRSTNAME);
+		String surname = user.getAttribute(AttributesConst.SURNAME);
+		String email = user.getAttribute(AttributesConst.EMAIL);
+
+		mainLayout.addFormItem(new Label(Strings.isNullOrEmpty(firstname) ? "" : firstname),
+				getTranslation("view.user-settings.profile.firstname"));
+		mainLayout.addFormItem(new Label(Strings.isNullOrEmpty(surname) ? "" : surname),
+				getTranslation("view.user-settings.profile.surname"));
+		mainLayout.addFormItem(new Label(Strings.isNullOrEmpty(email) ? "" : email),
+				getTranslation("view.user-settings.profile.email"));
+
+		getContent().add(mainLayout);
+	}
 }
