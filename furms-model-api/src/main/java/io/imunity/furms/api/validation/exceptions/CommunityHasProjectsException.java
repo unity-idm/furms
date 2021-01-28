f7a274141b836b83ff9cc813c349275007219727
Piotr Bihuniak <bihuniak.piotr@gmail.com>
2021-01-28 15:47:12 +0100

    FU-21 Q&A6

---
+++ furms-model-api/src/main/java/io/imunity/furms/api/validation/exceptions/CommunityHasProjectsException.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2020 Bixbit s.c. All rights reserved.
+ * See LICENSE file for licensing information.
+ */
+
+package io.imunity.furms.api.validation.exceptions;
+
+public class CommunityHasProjectsException extends RuntimeException{
+
+	public CommunityHasProjectsException(String message) {
+		super(message);
+	}
+}
