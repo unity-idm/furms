39f92944fb65ffc6f543247848573d4625e93440
Piotr Bihuniak <bihuniak.piotr@gmail.com>
2021-02-10 22:47:14 +0100

    FU-48 first version

--- furms-web-ui/src/main/java/io/imunity/furms/ui/views/community/CommunityAdminMenu.java
+++ furms-web-ui/src/main/java/io/imunity/furms/ui/views/community/CommunityAdminMenu.java
@@ -14,6 +14,7 @@ import io.imunity.furms.ui.components.FurmsAppLayout;
 import io.imunity.furms.ui.components.FurmsLayout;
 import io.imunity.furms.ui.components.MenuComponent;
 import io.imunity.furms.ui.user_context.RoleTranslator;
+import io.imunity.furms.ui.views.community.adminstrators.CommunityAdminsView;
 import io.imunity.furms.ui.views.community.projects.ProjectView;
 import io.imunity.furms.ui.views.community.projects.ProjectsView;
 import io.imunity.furms.ui.views.community.settings.SettingsView;
