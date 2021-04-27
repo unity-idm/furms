1eafa89d15e80dd260f3f2856280aed756fca604
Piotr Bihuniak <bihuniak.piotr@gmail.com>
2021-04-28 00:36:46 +0200

    FU-76 fix infinitive loops

--- furms-rabbitmq-site-model/src/main/java/io/imunity/furms/rabbitmq/site/models/converter/FurmsPayloadConverter.java
+++ furms-rabbitmq-site-model/src/main/java/io/imunity/furms/rabbitmq/site/models/converter/FurmsPayloadConverter.java
@@ -5,23 +5,28 @@
 
 package io.imunity.furms.rabbitmq.site.models.converter;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 import io.imunity.furms.rabbitmq.site.models.Payload;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.core.MessageProperties;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConversionException;
 import org.springframework.amqp.support.converter.MessageConverter;
 
-import java.io.UnsupportedEncodingException;
+import java.lang.invoke.MethodHandles;
 
 import static com.fasterxml.jackson.databind.SerializationFeature.WRITE_DATES_AS_TIMESTAMPS;
+import static java.util.Optional.ofNullable;
 
 public class FurmsPayloadConverter implements MessageConverter {
 	private static final String TYPE_ID = "__TypeId__";
+	private static final String UTF_8 = "UTF-8";
+	private static final Logger LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
 	public final ObjectMapper mapper;
 	public final Jackson2JsonMessageConverter jackson2JsonMessageConverter;
 
@@ -43,10 +48,13 @@ public class FurmsPayloadConverter implements MessageConverter {
 	@Override
 	public Object fromMessage(Message message) throws MessageConversionException {
 		try {
-			String contentAsString = new String(message.getBody(), message.getMessageProperties().getContentEncoding());
+			String contentEncoding = ofNullable(message.getMessageProperties().getContentEncoding()).orElse(UTF_8);
+			String contentAsString = new String(message.getBody(), contentEncoding);
 			return mapper.readValue(contentAsString, mapper.constructType(Payload.class));
-		} catch (JsonProcessingException | UnsupportedEncodingException e) {
-			throw new IllegalArgumentException("Message cannot be convert: ", e);
+		} catch (Exception e) {
+			LOG.info("Message cannot be convert: {}", message);
+			LOG.info("This error occurred when message was parsing: ", e);
+			return new Object();
 		}
 	}
 }
