0cd9a7916f8a055f94b38f42d4bace7aaa757e8c
Roman Krysinski <rkrysinski@gmail.com>
2021-04-28 11:51:36 +0200

    Python agent moved to separate repository: https://github.com/unity-idm/furms-python-agent

--- python-agent/clean.sh
+++
@@ -1,2 +0,0 @@
-#/bin/sh
-rm -rf venv furms-agent-venv build dist furms.egg-info html demo-agent/ssh_keys
--- python-agent/demo-agent/demo-agent.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-# Copyright (c) 2021 Bixbit s.c. All rights reserved.
-# See LICENSE file for licensing information.
-python3 agent/main.py "$@"
