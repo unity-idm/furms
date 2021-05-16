f0d725c6dad03534b74f025e2e1805821bf21c48
Piotr Piernik <piotrpiernik@gmail.com>
2021-05-16 13:49:42 +0200

    FU-200 CR-343 fixes

--- furms-web-ui/src/main/java/io/imunity/furms/ui/user_context/InvocationContext.java
+++ furms-web-ui/src/main/java/io/imunity/furms/ui/user_context/InvocationContext.java
@@ -3,7 +3,7 @@
  * See LICENCE.txt file for licensing information.
  */
 
-package io.imunity.furms.ui;
+package io.imunity.furms.ui.user_context;
 
 import java.io.Serializable;
 import java.time.ZoneId;
@@ -13,9 +13,16 @@ import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.vaadin.flow.component.UI;
 
 public class InvocationContext implements Serializable {
+
+	private static final Logger LOG = LoggerFactory.getLogger(InvocationContext.class);
+
+	private static final int ZONE_INIT_TIMEOUT = 30;
 
 	private final Future<ZoneId> zone;
 
@@ -30,25 +37,25 @@ public class InvocationContext implements Serializable {
 	public static InvocationContext getCurrent() {
 		InvocationContext ret = UI.getCurrent().getSession().getAttribute(InvocationContext.class);
 		if (ret == null)
-			throw new RuntimeException("The current call has no invocation context set");
+			throw new EmptyInvocationContextException("The current call has no invocation context set");
 		return ret;
 	}
 
 	public ZoneId getZone() {
 		try {
-			return zone.get(5, TimeUnit.SECONDS);
+			return zone.get(ZONE_INIT_TIMEOUT, TimeUnit.SECONDS);
 		} catch (InterruptedException | ExecutionException | TimeoutException e) {
-			throw new RuntimeException("Can not get user zone");
+			LOG.error("Can not get user zone");
 		}
+		return ZoneId.of("UTC");
 	}
 
-	public static void init() {
+	public static void init(UI ui) {
 		CompletableFuture<ZoneId> zone = new CompletableFuture<>();
-		UI.getCurrent().getPage().retrieveExtendedClientDetails(cd -> {
+		ui.getPage().retrieveExtendedClientDetails(cd -> {
 			zone.complete(ZoneId.of(cd.getTimeZoneId()));
 		});
 
 		setCurrent(new InvocationContext(zone));
 	}
-
 }
