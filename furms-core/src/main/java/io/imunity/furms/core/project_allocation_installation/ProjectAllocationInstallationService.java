892c6f3e45f61652c6044bb6f7526ea1cf16d58b
Piotr Bihuniak <bihuniak.piotr@gmail.com>
2021-05-25 15:25:00 +0200

    FU-76 CR

--- furms-core/src/main/java/io/imunity/furms/core/project_allocation_installation/ProjectAllocationInstallationService.java
+++ furms-core/src/main/java/io/imunity/furms/core/project_allocation_installation/ProjectAllocationInstallationService.java
@@ -14,6 +14,7 @@ import io.imunity.furms.spi.project_allocation_installation.ProjectAllocationIns
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.lang.invoke.MethodHandles;
 import java.util.Optional;
@@ -46,6 +47,7 @@ public class ProjectAllocationInstallationService {
 		return projectAllocationInstallationRepository.findAllDeallocation(projectId);
 	}
 
+	@Transactional
 	public void createAllocation(String projectAllocationId) {
 		CorrelationId correlationId = CorrelationId.randomID();
 		ProjectAllocationResolved projectAllocationResolved = projectAllocationRepository.findByIdWithRelatedObjects(projectAllocationId).get();
@@ -111,6 +113,7 @@ public class ProjectAllocationInstallationService {
 			projectAllocationInstallationRepository.findBySiteIdAndProjectAllocationId(projectDeallocation.siteId, projectDeallocation.projectAllocationId);
 		if(projectAllocationInstallation.status.isFailed()){
 			projectAllocationInstallationRepository.deleteBy(projectAllocationInstallation.id);
+			LOG.info("Deallocation was not created, because project allocation {} process failed", projectAllocationInstallation.id);
 			return;
 		}
 		projectAllocationInstallationRepository.create(projectDeallocation);
