5532bd9c8467909d204558c331bea8e204f59796
Piotr Bihuniak <bihuniak.piotr@gmail.com>
2021-05-26 17:49:38 +0200

    FU-231 first version

--- furms-web-ui/src/main/java/io/imunity/furms/ui/views/site/PolicyDocumentsView.java
+++ furms-web-ui/src/main/java/io/imunity/furms/ui/views/site/PolicyDocumentsView.java
@@ -5,13 +5,19 @@
 
 package io.imunity.furms.ui.views.site;
 
+import com.vaadin.flow.router.AfterNavigationEvent;
 import com.vaadin.flow.router.Route;
-import io.imunity.furms.ui.components.FurmsViewComponent;
+import io.imunity.furms.ui.components.FurmsLandingViewComponent;
 import io.imunity.furms.ui.components.PageTitle;
 
 import static io.imunity.furms.domain.constant.RoutesConst.SITE_BASE_LANDING_PAGE;
 
 @Route(value = SITE_BASE_LANDING_PAGE, layout = SiteAdminMenu.class)
 @PageTitle(key = "view.site-admin.policy-documents.page.title")
-public class PolicyDocumentsView extends FurmsViewComponent {
+public class PolicyDocumentsView extends FurmsLandingViewComponent {
+
+	@Override
+	public void afterNavigation(AfterNavigationEvent afterNavigationEvent) {
+		//reload page here
+	}
 }
