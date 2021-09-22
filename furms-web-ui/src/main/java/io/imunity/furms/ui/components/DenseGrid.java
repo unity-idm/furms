96de39e2fbc5914aa50eb539249a63b81c66539b
Roman Krysinski <rkrysinski@gmail.com>
2021-09-22 15:03:56 +0200

    FU-249: optimize dashbaords for narrowed browsers - introducing dense grid.

---
+++ furms-web-ui/src/main/java/io/imunity/furms/ui/components/DenseGrid.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2020 Bixbit s.c. All rights reserved.
+ * See LICENSE file for licensing information.
+ */
+package io.imunity.furms.ui.components;
+
+import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.grid.GridVariant;
+
+@CssImport(value = "./styles/components/dense-grid.css", themeFor = "vaadin-grid")
+public class DenseGrid<T> extends Grid<T> {
+
+	public DenseGrid(Class<T> beanType) {
+		super(beanType, false);
+		setDefaults();
+	}
+
+	private void setDefaults() {
+		setHeightByRows(true);
+		addThemeVariants(GridVariant.LUMO_NO_BORDER);
+		addThemeVariants(GridVariant.LUMO_WRAP_CELL_CONTENT);
+		addClassName("dense-grid");
+	}
+}
