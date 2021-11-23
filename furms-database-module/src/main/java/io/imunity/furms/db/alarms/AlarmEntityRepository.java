c1c4737ec8f8941de5e3a2089d46667d0899f5fe
Piotr Bihuniak <bihuniak.piotr@gmail.com>
2021-11-23 15:31:09 +0100

    FU-264 db init

---
+++ furms-database-module/src/main/java/io/imunity/furms/db/alarms/AlarmEntityRepository.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2020 Bixbit s.c. All rights reserved.
+ * See LICENSE file for licensing information.
+ */
+
+package io.imunity.furms.db.alarms;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.UUID;
+
+interface AlarmEntityRepository extends CrudRepository<AlarmEntity, UUID> {
+}
