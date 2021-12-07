81ffe0bd1b4a3e58892a44322e01f976be2d6407
bojan <patryk.bojanczyk@unity-idm.eu>
2021-12-07 16:45:44 +0100

    FU-150 - custom css loading via spring resource

--- furms-web-ui/src/main/java/io/imunity/furms/ui/config/CustomCSSProvider.java
+++ furms-web-ui/src/main/java/io/imunity/furms/ui/config/CustomCSSProvider.java
@@ -6,64 +6,68 @@
 package io.imunity.furms.ui.config;
 
 import com.vaadin.flow.component.UI;
-import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.FileSystemResource;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.io.IOException;
 import java.lang.invoke.MethodHandles;
-import java.nio.charset.StandardCharsets;
 
 import static org.apache.logging.log4j.util.Strings.isEmpty;
+import static org.springframework.util.ResourceUtils.FILE_URL_PREFIX;
 
-@Component
-public class CustomCSSProvider {
+@Configuration
+public class CustomCSSProvider implements WebMvcConfigurer {
 	private static final Logger LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
-	private final static String CUSTOM_CSS_PATH = "META-INF/resources/frontend/styles/custom-dynamic.css";
-	private final static String CUSTOM_CSS_FRONTEND_PATH = "./styles/custom-dynamic.css";
+	private final Resource externalCSSResource;
 
-	private final FileSystemResource externalCSSResource;
-	private final ClassPathResource customCSSResource;
-
-	public CustomCSSProvider(@Value("${furms.front.layout.styles.custom}") FileSystemResource externalCSSResource,
-	                         @Value(CUSTOM_CSS_PATH) ClassPathResource customCSSResource) {
+	public CustomCSSProvider(@Value("${furms.front.layout.styles.custom}") Resource externalCSSResource) {
 		this.externalCSSResource = externalCSSResource;
-		this.customCSSResource = customCSSResource;
+	}
+
+	@Override
+	public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+		if (externalCSSResource != null) {
+			try {
+				if (isCustomCssFileAvailable()) {
+					final String extraStyleResourceLocation = createResourceLocationsFrom(externalCSSResource);
+					registry.addResourceHandler("/extra-style/{filename:(?:" + externalCSSResource.getFilename() + ")}")
+							.addResourceLocations(extraStyleResourceLocation);
+				} else {
+					LOG.error("Custom CSS File is not available to load and configure ({})", externalCSSResource);
+				}
+			} catch (IOException exception) {
+				LOG.error("Could not load custom CSS file: ", exception);
+			}
+		}
 	}
 
 	public void initAndAttach(final UI uiToAttachStyle) {
 		try {
-			if (isExternalResourceExists()) {
-				writeToCSS(new String(externalCSSResource.getInputStream().readAllBytes()));
-			} else {
-				clearCSS();
+			if (externalCSSResource != null && isCustomCssFileAvailable()) {
+				uiToAttachStyle.getPage().addStyleSheet("/extra-style/" + externalCSSResource.getFilename());
 			}
-			uiToAttachStyle.getPage().addStyleSheet(CUSTOM_CSS_FRONTEND_PATH);
 		} catch (final IOException exception) {
 			LOG.error("Could not load custom CSS file: ", exception);
 		}
 	}
 
-	private boolean isExternalResourceExists() {
-		return externalCSSResource != null
-				&& !isEmpty(externalCSSResource.getPath())
+	private boolean isCustomCssFileAvailable() throws IOException {
+		return !isEmpty(externalCSSResource.getURL().getPath())
 				&& externalCSSResource.exists()
 				&& externalCSSResource.isReadable();
 	}
 
-	private void clearCSS() throws IOException {
-		writeToCSS("");
-	}
-
-	private void writeToCSS(String content) throws IOException {
-		FileUtils.write(
-				customCSSResource.getFile(),
-				content,
-				StandardCharsets.UTF_8);
+	private String createResourceLocationsFrom(Resource externalCSSResource) throws IOException {
+		final String prefix = !externalCSSResource.getURL().getPath().startsWith(FILE_URL_PREFIX)
+				? FILE_URL_PREFIX : "";
+		final String suffix = !externalCSSResource.getFile().getParent().endsWith("/")
+				? "/" : "";
+		return prefix + externalCSSResource.getFile().getParent() + suffix;
 	}
 }
