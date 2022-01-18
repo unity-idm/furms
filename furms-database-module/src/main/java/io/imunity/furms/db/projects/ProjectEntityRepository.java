20ea3cc0ff40e1f1b84ea2c408a779578a5c06cd
Piotr Bihuniak <bihuniak.piotr@gmail.com>
2022-01-19 00:08:54 +0100

    FU-317/331 upgrade deps

--- furms-database-module/src/main/java/io/imunity/furms/db/projects/ProjectEntityRepository.java
+++ furms-database-module/src/main/java/io/imunity/furms/db/projects/ProjectEntityRepository.java
@@ -7,12 +7,12 @@ package io.imunity.furms.db.projects;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Set;
 import java.util.UUID;
-import java.util.stream.Stream;
 
 interface ProjectEntityRepository extends CrudRepository<ProjectEntity, UUID> {
 
-	Stream<ProjectEntity> findAllByCommunityId(UUID communityId);
+	Set<ProjectEntity> findAllByCommunityId(UUID communityId);
 	boolean existsByCommunityIdAndName(UUID communityId, String name);
 	boolean existsByCommunityIdAndId(UUID communityId, UUID id);
 }
