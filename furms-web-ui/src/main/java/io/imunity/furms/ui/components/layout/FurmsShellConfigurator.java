9decacc61319376829e48bafd55bb5cdf81a61e7
Piotr Bihuniak <bihuniak.piotr@gmail.com>
2022-01-27 13:58:39 +0100

    vaadin 22 upgrade

---
+++ furms-web-ui/src/main/java/io/imunity/furms/ui/components/layout/FurmsShellConfigurator.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2020 Bixbit s.c. All rights reserved.
+ * See LICENSE file for licensing information.
+ */
+
+package io.imunity.furms.ui.components.layout;
+
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.component.page.Push;
+import com.vaadin.flow.theme.Theme;
+import com.vaadin.flow.theme.lumo.Lumo;
+
+@Push
+@Theme(themeClass = Lumo.class)
+class FurmsShellConfigurator implements AppShellConfigurator {
+}
