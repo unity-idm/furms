67a6d7445adb3f05a92039571a33f87e72255a8e
Piotr Bihuniak <bihuniak.piotr@gmail.com>
2022-06-07 13:31:38 +0200

    FU-32 change name of env

--- furms-end-to-end-tests/src/test/java/io/imunity/furms/end_to_end/tests/FurmsUIChromeDriverFactory.java
+++ furms-end-to-end-tests/src/test/java/io/imunity/furms/end_to_end/tests/FurmsUIChromeDriverFactory.java
@@ -11,7 +11,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 
 public class FurmsUIChromeDriverFactory {
 	public static FurmsUIDriver<?> create(){
-		String getenv = System.getenv("chrome-driver-path");
+		String getenv = System.getenv("CHROME_DRIVER_PATH");
 		System.setProperty("webdriver.chrome.driver", getenv);
 
 		ChromeOptions options = new ChromeOptions();
